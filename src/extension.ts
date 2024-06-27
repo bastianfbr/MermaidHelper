3ec7d38042dc7902e0e2dd7515632eb471e0bfb1
Bastian <badmaxyt@gmail.com>
2024-06-27 23:15:23 +0200

    refactor: :recycle: use extracted logic and interfaces

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,6 @@
 import * as vscode from 'vscode';
+import { getLinkIndices, getLinkStyles } from './utils/parser';
+import { createDecorationOptions } from './utils/decoration';
 
 let idDecorationType: vscode.TextEditorDecorationType;
 
@@ -11,37 +13,32 @@ export function activate(context: vscode.ExtensionContext) {
         }
     });
 
+    const updateActiveEditorDecorations = () => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            updateDecorations(editor);
+        }
+    };
+
     if (vscode.window.activeTextEditor) {
-        updateDecorations(vscode.window.activeTextEditor);
+        updateActiveEditorDecorations();
     }
 
     context.subscriptions.push(
-        vscode.window.onDidChangeActiveTextEditor(editor => {
-            if (editor) {
-                updateDecorations(editor);
-            }
-        }),
-
+        vscode.window.onDidChangeActiveTextEditor(updateActiveEditorDecorations),
         vscode.workspace.onDidOpenTextDocument(document => {
             const editor = vscode.window.visibleTextEditors.find(e => e.document === document);
             if (editor) {
                 updateDecorations(editor);
             }
         }),
-
         vscode.workspace.onDidChangeTextDocument(event => {
             const editor = vscode.window.visibleTextEditors.find(e => e.document === event.document);
             if (editor) {
                 updateDecorations(editor);
             }
         }),
-
-        vscode.commands.registerCommand('extension.annotateMermaid', () => {
-            const editor = vscode.window.activeTextEditor;
-            if (editor) {
-                updateDecorations(editor);
-            }
-        })
+        vscode.commands.registerCommand('extension.annotateMermaid', updateActiveEditorDecorations)
     );
 }
 
@@ -58,104 +55,7 @@ function updateDecorations(editor: vscode.TextEditor) {
     const linkIndices = getLinkIndices(text);
     const linkStyles = getLinkStyles(text);
 
-    const decorations: vscode.DecorationOptions[] = [];
-
-    // Add link ID decorations
-    for (const linkIndex of linkIndices) {
-        const startPos = document.positionAt(linkIndex.index);
-        const endPos = document.positionAt(linkIndex.index + linkIndex.length);
-        const range = new vscode.Range(startPos, endPos);
-
-        const decoration = {
-            range: range,
-            renderOptions: {
-                after: {
-                    contentText: `ID: ${linkIndex.id}`
-                }
-            }
-        };
-
-        decorations.push(decoration);
-    }
-
-    // Add linkStyle decorations with color coding
-    for (const linkStyle of linkStyles) {
-        const startPos = document.positionAt(linkStyle.index);
-        const endPos = document.positionAt(linkStyle.index + linkStyle.length);
-        const range = new vscode.Range(startPos, endPos);
-
-        const relatedLink = linkIndices.find(link => link.id === linkStyle.id);
-        if (relatedLink) {
-            const colorMatch = /stroke:(#[0-9A-Fa-f]{6})/.exec(linkStyle.line);
-            const color = colorMatch ? colorMatch[1] : 'gray';
-
-            const decoration = {
-                range: range,
-                renderOptions: {
-                    after: {
-                        contentText: `Relation: ${relatedLink.line}`,
-                        color: color
-                    }
-                }
-            };
-
-            decorations.push(decoration);
-        }
-    }
+    const decorations = createDecorationOptions(linkIndices, linkStyles, document);
 
     editor.setDecorations(idDecorationType, decorations);
-}
-
-interface LinkIndex {
-    index: number;
-    length: number;
-    id: number;
-    line: string;
-}
-
-interface LinkStyle {
-    index: number;
-    length: number;
-    id: number;
-    line: string;
-}
-
-function getLinkIndices(text: string): LinkIndex[] {
-    const lines = text.split('\n');
-    let linkIndex = 0;
-    const linkIndices: LinkIndex[] = [];
-
-    for (const line of lines) {
-        const match = /(-->|---)/.exec(line);
-        if (match) {
-            linkIndices.push({
-                index: text.indexOf(line),
-                length: line.length,
-                id: linkIndex,
-                line: line
-            });
-            linkIndex++;
-        }
-    }
-
-    return linkIndices;
-}
-
-function getLinkStyles(text: string): LinkStyle[] {
-    const lines = text.split('\n');
-    const linkStyles: LinkStyle[] = [];
-
-    for (const line of lines) {
-        const match = /linkStyle (\d+)/.exec(line);
-        if (match) {
-            linkStyles.push({
-                index: text.indexOf(line),
-                length: line.length,
-                id: parseInt(match[1]),
-                line: line
-            });
-        }
-    }
-
-    return linkStyles;
 }
